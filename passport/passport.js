5f243fa3bd850f7fe9e7b321818f0b26d356e1d9
Jean Renel Fanfan <131271711+jrfanfan@users.noreply.github.com>
2024-06-29 12:14:23 -0400

    Update passport.js

--- passport/passport.js
+++ passport/passport.js
@@ -7,7 +7,7 @@ module.exports = module.exports = function(passport) {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://cse341-l08-xgpr.onrender.com/auth/google/callback'
+      callbackURL: '/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     const newUser = {
